4239d3fd93a43110d5b05c1bdc47f720a3d70db7
Eugene <eugenemutuyimana15@gmail.com>
2024-06-19 20:51:06 +0200

    product urls

---
+++ product/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import home, check_out, add_product, add_to_cart, view_cart
+
+urlpatterns=[
+    path('', home, name='home'),
+    path('check-out/', check_out, name='checkout'),
+    path('upload-product/',add_product, name='add_product'),
+    path('add-t-cart/', add_to_cart, name='add_to_cart'),
+    path('view-cart/', view_cart, name='view_cart')
+]
