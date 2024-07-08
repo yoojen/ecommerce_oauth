3831a07d5727259aa56dfa4863cac69632176930
Eugene <eugenemutuyimana15@gmail.com>
2024-07-08 22:54:44 +0200

    added new urls

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, check_out, add_product, add_to_cart, view_cart
+from .views import home, check_out, add_product, add_to_cart, view_cart, orders
 
 urlpatterns=[
     path('', home, name='home'),
@@ -7,4 +7,5 @@ urlpatterns=[
     path('upload-product/',add_product, name='add_product'),
     path('add-t-cart/', add_to_cart, name='add_to_cart'),
     path('view-cart/', view_cart, name='view_cart'),
+    path('orders/', orders, name='orders'),
 ]
